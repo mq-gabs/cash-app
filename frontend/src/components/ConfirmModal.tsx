7c80db4e38b599ad1b3a1c80e565553f138fd1e7
gabrieu <gabriel.marques@hublocal.com.br>
2024-11-24 17:22:10 -0300

    feat: create modal and confirm modal

---
+++ frontend/src/components/ConfirmModal.tsx
@@ -0,0 +1,51 @@
+import clsx from "clsx";
+import Button from "./Button";
+import Modal from "./Modal";
+
+interface IConfirmModal {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  title: string;
+  text: string;
+  type?: 'default' | 'warning';
+}
+
+export default function ConfirmModal({
+  onClose,
+  onConfirm,
+  open,
+  title,
+  text,
+  type = 'default',
+}: IConfirmModal) {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      title={title}
+    >
+      <div>
+        <div className="mb-4">
+          <p>{text}</p>
+        </div>
+        <div className="flex gap-2 w-full justify-between">
+          <Button
+            onClick={onClose}
+            className="bg-gray-300 text-gray-950"
+          >
+            Cancelar
+          </Button>
+          <Button
+            onClick={onConfirm}
+            className={clsx({
+              "bg-red-400": type === 'warning',
+            })}
+          >
+            Confirmar
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,39 @@
+import { RxCrossCircled } from "react-icons/rx";
+import Button from "./Button";
+
+interface IModal {
+  open: boolean;
+  onClose: () => void;
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function Modal({
+  onClose,
+  open,
+  title = 'Título',
+  children,
+}: IModal) {
+  if (!open) return <></>;
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-10">
+      <div className="absolute w-full h-full bg-gray-500 opacity-50"/>
+      <div className="bg-white z-20 rounded">
+        <div className="bg-primary w-full p-4 flex justify-between gap-2 rounded-tl rounded-tr">
+          <p className="text-white font-bold">
+            {title}
+          </p>
+          <Button
+            onClick={onClose}
+          >
+            <RxCrossCircled size={20} />
+          </Button>
+        </div>
+        <div className="p-4">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
