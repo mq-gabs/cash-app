089142c3cc5d7c1c3c85142cab0577c272072975
gabrieu <gabriel.marques@hublocal.com.br>
2024-11-27 09:25:21 -0300

    feat: change currency formatation

--- frontend/src/utils/formaters.ts
+++ frontend/src/utils/formaters.ts
@@ -12,8 +12,41 @@ export function unformatDate(date: string) {
   return new Date(date.split('/').reverse().join('-'));
 }
 
+function insertDots(value: string) {
+  const sValue = value.split('').reverse();
+
+  const SIZE = 3;
+  const N_SLICES = Math.ceil(value.length / SIZE);
+
+  const splited = [...Array(N_SLICES)].map((_, index) => {
+    return sValue.slice(index * SIZE, (index + 1) * SIZE);
+  });
+
+  const rejoin = splited.map(v => v.reverse().join('')).reverse().join('.');
+
+  return rejoin;
+}
+
 export function formatCurrency(value: number) {
-  return `R$ ${(value / 100).toFixed(2)}`;
+  let isNegative = value < 0;
+
+  if (isNegative) {
+    value = Math.abs(value);
+  }
+
+  const fixedValue = (value / 100).toFixed(2);
+
+  const [int, dec] = fixedValue.split('.');
+
+  const intWithDots = insertDots(int);
+
+  let finalValue = [intWithDots, dec].join(',')
+
+  if (isNegative) {
+    finalValue = `-${finalValue}`;
+  }
+
+  return `R$ ${finalValue}`;
 }
 
 export function formatPercentage(value: number) {
